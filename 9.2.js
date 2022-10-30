ecacf5011113c061087151d332cd7289e9682889
AlexandrTsy <114476920+AlexandrTsy@users.noreply.github.com>
2022-10-30 13:47:04 +0300

    Create 9.2.js

---
+++ 9.2.js
@@ -0,0 +1,15 @@
+console.log(JSON.parse(`
+  {
+    "list": [
+      {
+      "name": "Petr",
+      "age": "20",
+      "prof": "mechanic"
+      },
+      {
+      "name": "Vova",
+      "age": "60",
+      "prof": "pilot"
+      }
+    ]
+  }`))
